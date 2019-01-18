2a4c5d257359ade5bce3ad0f04311b9ef1ddc3d9
Eric Zhang <ekzhang1@gmail.com>
2019-01-17 23:44:09 -0600

    Add database integration, save old tracks

--- app.py
+++ app.py
@@ -1,32 +1,56 @@
 import os
 from fractions import Fraction
 
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 from voicing import generateChorale
 
 app = application = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    else:
-        ts = request.form['ts'] or None
-        chorale = request.form['chorale']
-        rhythm = list(filter(None, request.form['rhythm'].split()))
-        lengths = None
-        if rhythm:
-            lengths = [Fraction(rhythm[i % len(rhythm)])
-                       for i in range(len(chorale))]
+class Chorale(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mxml = db.Column(db.Text(), nullable=False)
+    timesig = db.Column(db.Text())
+    rhythm = db.Column(db.Text())
+    chorale = db.Column(db.Text())
 
-        score = generateChorale(chorale, lengths, ts)
-        fp = score.write('musicxml')
-        with open(fp, 'r') as f:
-            mxml = f.read()
-            return render_template('index.html', mxml=mxml, ts=ts,
-                                   rhythm=' '.join(rhythm), chorale=chorale)
+    def __repr__(self):
+        return '<Chorale {}>'.format(self.id)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    ts = request.form['ts'] or None
+    chorale = request.form['chorale']
+    rhythm = list(filter(None, request.form['rhythm'].split()))
+    lengths = None
+    if rhythm:
+        lengths = [Fraction(rhythm[i % len(rhythm)])
+                    for i in range(len(chorale))]
+
+    score = generateChorale(chorale, lengths, ts)
+    fp = score.write('musicxml')
+    with open(fp, 'r') as f:
+        mxml = f.read()
+        obj = Chorale(mxml=mxml, timesig=ts,
+                        rhythm=' '.join(rhythm), chorale=chorale)
+        db.session.add(obj)
+        db.session.commit()
+        return redirect(url_for('view', chorale_id=obj.id))
+
+
+@app.route('/view/<int:chorale_id>')
+def view(chorale_id):
+    chorale = Chorale.query.get(chorale_id)
+    return render_template('index.html', mxml=chorale.mxml, ts=chorale.timesig,
+                           rhythm=chorale.rhythm, chorale=chorale.chorale)
