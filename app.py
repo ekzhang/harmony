37bee6af956ae77c8931ef042c1b4bcca4179ed8
Eric Zhang <ekzhang1@gmail.com>
2019-01-17 23:11:22 -0600

    Sqlalchemy boilerplate

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, request, render_template
+import os
 from fractions import Fraction
+
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 from voicing import generateChorale
 
 app = application = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
