303c7ef50f90d61aa7946cbc6be4cacae94548c9
Eric Zhang <ekzhang1@gmail.com>
2020-08-18 21:24:29 -0500

    Formatting & Fraction

--- voicing.py
+++ voicing.py
@@ -1,6 +1,8 @@
 import copy
 import argparse
 import itertools
+from fractions import Fraction
+
 from music21.note import Note
 from music21.pitch import Pitch
 from music21.chord import Chord
@@ -244,40 +246,38 @@ def generateChorale(chorale, lengths=None, ts="4/4"):
 
 def main():
     parser = argparse.ArgumentParser(
-        description=('Generates four-part harmony with idiomatic '
-                     'voice-leading procedures and dynamic programming.'))
-    parser.add_argument(
-        'key', type=str, nargs='?',
-        help=('the key of the chord progression')
+        description="Generates four-part harmony with idiomatic "
+        "voice-leading procedures and dynamic programming."
     )
     parser.add_argument(
-        'chord_progression', type=str, nargs='?',
-        help=('a sequence of roman numeral annotations'
-              ', e.g., "I I6 IV V43/ii ii V V7 I"')
+        "key", type=str, nargs="?", help="the key of the chord progression",
     )
     parser.add_argument(
-        'durations', type=str, nargs='?',
-        help=('the associated durations of the chords (in quarter notes)')
+        "chord_progression",
+        type=str,
+        nargs="?",
+        help='a sequence of roman numeral annotations, e.g., "I I6 IV V43/ii ii V V7 I"',
     )
     parser.add_argument(
-        'time_signature', type=str, nargs='?',
-        help=('the time signature')
+        "durations",
+        type=str,
+        nargs="?",
+        help="the associated durations of the chords (in quarter notes)",
+    )
+    parser.add_argument(
+        "time_signature", type=str, nargs="?", help="the time signature"
     )
     parser.set_defaults(
-        key='B-',
-        chord_progression='I I6 IV V43/ii ii V V7 I',
-        durations='1 1/2 1 1/2 1 1/2 1/2 1',
-        time_signature='6/8',
+        key="B-",
+        chord_progression="I I6 IV V43/ii ii V V7 I",
+        durations="1 1/2 1 1/2 1 1/2 1/2 1",
+        time_signature="6/8",
     )
     args = parser.parse_args()
-    key_and_chords = f'{args.key}: {args.chord_progression}'
-    durations = [eval(x) for x in args.durations.split()]
+    key_and_chords = f"{args.key}: {args.chord_progression}"
+    durations = [Fraction(x) for x in args.durations.split()]
     time_signature = args.time_signature
-    generateChorale(
-        key_and_chords,
-        durations,
-        time_signature
-    ).show()
+    generateChorale(key_and_chords, durations, time_signature).show()
 
 
 if __name__ == "__main__":
